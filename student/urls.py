e64b7fdb2ea479c058e475f3ef9dd0d3fe1672dd
Suryash <joblesscoder2468@gmail.com>
2022-11-06 01:17:16 +0530

    Initial Setup

---
+++ student/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('certificateUpload/<str:id>/', views.certificateUpload, name='certificateUpload'),
+    path('login/', views.login, name='login'),
+    path('signUp/', views.certificateUpload, name='certificateUpload'),
+    path('profile/<str: id>/', views.certificateUpload, name='certificateUpload'),
+]
